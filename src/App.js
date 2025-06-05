44b52bf8c117f86baa1ca74883ddb640c47708bf
rv810 <vanir1@mcmaster.ca>
2025-06-05 16:01:34 -0400

    modified frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Timer from "./components/timer";
 
+
 function App() {
     return (
-        <div>
+        <div style={{
+            backgroundColor: "pink",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        }}>
             <Timer></Timer>
         </div>
     );
 }
+
 
 export default App;
--- src/components/timer.js
+++ src/components/timer.js
@@ -1,15 +1,83 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {Typography} from '@mui/material'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button'
+
+
+
 
 const Timer = () =>{
-  let time  = new Date().toLocaleTimeString()
+  const [timerActive, setTimerActive] = useState(false)
+  const [time, setTime] = useState(1500)
 
-  const [ctime,setTime] = useState(time)
-  const UpdateTime=()=>{
-    time =  new Date().toLocaleTimeString()
-    setTime(time)
-  }
-  setInterval(UpdateTime)
-  return <Typography fontWeight="600" align="center">{ctime}</Typography>
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timerActive) {
+        setTime(time => time-1)
+      }
+    }, 1000)
+
+
+    return () => clearInterval(interval)
+  }, [timerActive])
+
+
+  const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+  const seconds = (time % 60).toString().padStart(2, '0');
+
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "white",
+        width: "50vh",
+        height:"25vh",
+        borderRadius: 5,
+        display: "flex",
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection:"column"
+      }}>
+      <Typography fontWeight="600" align="center" color="black" display="flex" justifyContent="center" marginBottom="20px" fontSize="50px">
+        {minutes} : {seconds}
+      </Typography>
+      <div
+        sx={{
+          display: "flex",
+          alignItems:"center",
+          justifyContent:"center",
+          flexDirection: "row",
+        }}
+      >
+        <Button
+          sx={{
+            backgroundColor: "pink",
+            color: "black",
+            marginRight: "5px"
+          }}
+          onClick={() => {
+            setTimerActive(true);
+        }}>
+          Start Timer
+        </Button>
+        <Button
+          sx={{
+              backgroundColor: "pink",
+              color: "black",
+            }}
+          onClick={() => {
+            setTimerActive(false);
+            setTime(1500);
+          }
+        }>
+          Reset Timer
+      </Button>
+
+
+      </div>
+    </Box>
+   
+  )
 }
 export default Timer
